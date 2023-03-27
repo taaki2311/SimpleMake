1bda691d36dc951c235370a93ae11f619548e744
Tarun Singh <tsingh@alarm.com>
2023-03-27 11:59:42 -0400

    Adding multiple files

---
+++ TestProject2/src/file2.c
@@ -0,0 +1,5 @@
+#include "testProject2.h"
+
+int add_2(int a, int b) {
+    return a + b;
+}
--- TestProject2/src/test_project2.c
+++ TestProject2/src/test_project2.c
@@ -1,6 +1,7 @@
+#include "testProject2.h"
 #include <stdio.h>
 
 int main() {
-    printf("Hello, World!");
+    printf("Hello, World! The number is %d\n", add2(1, 2));
     return 0;
 }
