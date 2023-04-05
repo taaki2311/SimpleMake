9c0c40d424a40110d6bd4f48afe4f45fd9e23d14
taaki <taaki.paul@gmail.com>
2023-04-05 18:05:02 -0400

    Got rid of libraries

--- TestLibrary2/include/spacer/message.h
+++
@@ -1,6 +0,0 @@
-#ifndef MESSAGE_H
-#define MESSAGE_H
-
-#define MESSAGE "Hello from lib 2!"
-
-#endif
--- TestLibrary2/lib2.c
+++
@@ -1,9 +0,0 @@
-#include "lib2.h"
-#include "message.h"
-
-#include "libTest.h"
-
-void hello_from_lib2(void)
-{
-    super_print(MESSAGE);
-}
