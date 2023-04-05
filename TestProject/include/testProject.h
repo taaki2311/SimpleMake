47d59a793bd7bfac8357567abfd22dd25f6d720c
taaki <taaki.paul@gmail.com>
2023-04-05 18:12:06 -0400

    syncing

---
+++ TestProject/include/testProject.h
@@ -0,0 +1,6 @@
+#ifndef TEST_PROJECT_H
+#define TEST_PROJECT_H
+
+void hello_from_project(void);
+
+#endif
---
+++ TestProject/source/hello/hello_from_project.c
@@ -0,0 +1,8 @@
+#include "testProject.h"
+
+#include <libTest.h>
+
+void hello_from_project(void)
+{
+    super_print("Hello from Project!");
+}
