9465494f56cbd82bf5dd4ca79b5dcf1e131743b2
taaki <taaki.paul@gmail.com>
2023-04-07 11:55:56 -0400

    Got dynamic -> dynamic -> working

--- library.mk
+++ library.mk
@@ -4,10 +4,11 @@ DYNAMIC ?= $(DYNAMIC_DIR)/lib$(NAME).so
 SOURCES += $(foreach SRC_DIR, $(SRC_DIRS), $(wildcard $(SRC_DIR)/*.c))
 INCLUDES += $(addprefix -I, $(INC_DIRS))
 
-LIB_DIRS += $(dir $(STATIC_LIBS))
+LIB_DIRS += $(dir $(STATIC_LIBS)) $(DYNAMIC_LIB_DIRS)
 LIB_NAMES += $(patsubst lib%.a, %, $(notdir $(STATIC_LIBS)))
 
-LDFLAGS += $(addprefix -L, $(LIB_DIRS))
+COMMA := ,
+LDFLAGS += $(addprefix -L, $(LIB_DIRS)) $(addprefix -Wl$(COMMA)-R, $(DYNAMIC_LIB_DIRS))
 LDLIBS += $(addprefix -l, $(LIB_NAMES))
 
 OBJECTS += $(patsubst %.c, $(OBJ_DIR)/%.o, $(SOURCES))
--- project/source/main.c
+++ project/source/main.c
@@ -1,8 +1,6 @@
-#include "hello_world.h"
 #include "lib3.h"
 
 int main() {
-    hello_world();
     hello_from_lib3();
     return 0;
 }
